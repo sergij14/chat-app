70dd87b93eb8e6165b48ef94d35be07d0d879ea8
sergij14 <sergi.jajanidze@gmail.com>
2022-10-31 10:03:32 +0400

    connected client to the ws server

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 const http = require("http");
-const io = require("socket.io");
+const socketio = require("socket.io");
 dotenv.config({ path: "./.env" });
 
 const app = express();
 const server = http.createServer(app);
-const port = process.env.PORT || 8000;
+const io = socketio(server)
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', () => {
-  console.log('new sw connection');
+  console.log('new ws connection');
 })
 
 server.listen(port, console.log("server is running on port " + port));
