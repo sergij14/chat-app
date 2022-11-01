d55de8db69fb7b0536792462354aee2770c2dd32
sergij14 <sergi.jajanidze@gmail.com>
2022-11-01 20:40:55 +0400

    modified the rendering of active chat rooms select element

--- public/index.js
+++ public/index.js
@@ -2,10 +2,7 @@ const socket = io({ query: { indexPage: true } });
 
 // elements
 const $chatActiveRoomSelectWrapper = document.querySelector(
-    "#chat-active-room-select-wrapper"
-  );
-const $chatActiveRoomSelect = document.querySelector(
-  "#chat-active-room-select"
+  "#chat-active-room-select-wrapper"
 );
 const $chatRoomInput = document.querySelector("#chat-room-input");
 
@@ -14,35 +11,39 @@ const activeRoomTemplate = document.querySelector(
   "#active-room-template"
 ).innerHTML;
 
-$chatActiveRoomSelect.addEventListener("change", (evt) => {
-  const { value } = evt.target;
-  if (value) {
-    $chatRoomInput.removeAttribute("required");
-    $chatRoomInput.removeAttribute("room");
-    $chatRoomInput.setAttribute("disabled", "disabled");
-
-    $chatActiveRoomSelect.setAttribute("name", "room");
-  } else {
-    $chatRoomInput.setAttribute("required", "required");
-    $chatRoomInput.setAttribute("room", "room");
-    $chatRoomInput.removeAttribute("disabled");
-
-    $chatActiveRoomSelect.removeAttribute("name");
-  }
-});
-
 socket.on("room_list", (rooms) => {
-  if (rooms.length) {
-    $chatActiveRoomSelectWrapper.classList.remove("is-hidden");
-  } else {
-    $chatActiveRoomSelectWrapper.classList.add("is-hidden");
-  }
-
-  const html = Mustache.render(activeRoomTemplate, { rooms });
-
-  Array.from($chatActiveRoomSelect.children).forEach((node) => {
-    if (node.value) $chatActiveRoomSelect.removeChild(node);
+  const html = Mustache.render(activeRoomTemplate, {
+    rooms,
+    roomsCount: rooms.length,
   });
 
-  $chatActiveRoomSelect.insertAdjacentHTML("beforeend", html);
+  $chatActiveRoomSelectWrapper.innerHTML = "";
+  $chatActiveRoomSelectWrapper.insertAdjacentHTML("beforeend", html);
+
+  const $chatActiveRoomSelect = document.querySelector(
+    "#chat-active-room-select"
+  );
+
+  if (!rooms.length) {
+    $chatActiveRoomSelect.setAttribute("disabled", "disabled");
+  } else {
+    $chatActiveRoomSelect.removeAttribute("disabled");
+  }
+
+  $chatActiveRoomSelect.addEventListener("change", (evt) => {
+    const { value } = evt.target;
+    if (value) {
+      $chatRoomInput.removeAttribute("required");
+      $chatRoomInput.removeAttribute("room");
+      $chatRoomInput.setAttribute("disabled", "disabled");
+
+      $chatActiveRoomSelect.setAttribute("name", "room");
+    } else {
+      $chatRoomInput.setAttribute("required", "required");
+      $chatRoomInput.setAttribute("room", "room");
+      $chatRoomInput.removeAttribute("disabled");
+
+      $chatActiveRoomSelect.removeAttribute("name");
+    }
+  });
 });
